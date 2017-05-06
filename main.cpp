9904bbf204ad56e57d6f6b88d6f669b6058ff256
Carlos <alfreoldoriverac25@gmail.com>
2017-05-06 09:11:08 -0600

    imprimir tablero

--- main.cpp
+++ main.cpp
@@ -4,32 +4,52 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
 
+struct juego{
+    char** tablero;
+    char p1;
+    char p2;
+};
+void imprimirtablero(juego p, int f,int c);
+//bool jugar(juego p , bool juegoganado , int f, int c, int piezas);
+
+int main(int argc, char* argv[]){
+    juego p;
+    int f,c, piezas;
+    bool juegoganado  =false;
+    f = 6;
+    c = 7;
+    piezas = 4;
+    p.tablero = new char*[f];
+    for(int i = 0; i < f; i ++){
+        p.tablero[i] =  new char[c];
+    }
+    for(int j = 0;j < f;j++){
+        for(int k = 0;k<j;k++){
+            p.tablero[j][k];
+        }
+    }
+    imprimirtablero(p,f,c);
+       // juegoganado = jugar(p, juegoganado, f, c, piezas); 
     return 0;
 }
 
-void imprimirmatriz(int size,char** matriz){
-    for(int i = 0;i < size;i++){
-        for(int j = 0;j < size; j++){
-            cout<<" "<<matriz[i][j];
+void  imprimirtablero(juego p, int f, int c){
+    cout<<endl;
+    for(int i = 0; i < f;i++){
+        for(int j = 0; j < c; j++){
+            cout<<"|"<<p.tablero[i][j];
+            cout<<"|";
+            cout<<endl;
+            for(int k = 0; k < c; k++){
+                cout<<"--";
+                cout<<endl;
+            }
         }
-        cout<<endl;
-    }
-}
-
-char** crearmatriz(int n){
-    char** retval = new char*[n];
-
-    for(int i=0;i<n;i++){
-        retval[i]= new char[n];
-    }
-    return retval;
-}
-
-void liberarmatriz(int n,char** matriz){
-    for(int i=0;i<n;i++){
-        delete[] matriz[i];
     }
     
 }
+
+bool jugar();
+
+
