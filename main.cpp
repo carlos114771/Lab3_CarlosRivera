c7b9f2e9b129529329874ee803c05e110e7c86a4
Carlos <alfreoldoriverac25@gmail.com>
2017-05-06 10:13:09 -0600

    metodo soltar

--- main.cpp
+++ main.cpp
@@ -28,12 +28,12 @@ int main(int argc, char* argv[]){
         p.tablero[i] =  new char[c];
     }
     for(int j = 0;j < f;j++){
-        for(int k = 0;k<j;k++){
-            p.tablero[j][k];
+        for(int k = 0;k<c;k++){
+            p.tablero[j][k]= ' ';
         }
     }
     imprimirtablero(p,f,c);
-       // juegoganado = jugar(p, juegoganado, f, c, piezas); 
+       juegoganado = jugar(p, juegoganado, f, c, piezas); 
     return 0;
 }
 
@@ -47,6 +47,7 @@ void  imprimirtablero(juego p, int f, int c){
             for(int k = 0; k < c; k++){
                 cout<<"--";
                 cout<<endl;
+                
             }
         }
     }
@@ -54,34 +55,83 @@ void  imprimirtablero(juego p, int f, int c){
 }
 
 bool jugar(juego p, bool juegoganado, int f, int c, int piezas){
-    int columna, agarrar = 0; colocar=0;
+    int columna, agarrar = 0, colocar=0;
     char jugador = 'y';
-    while(!juegoterminado){
+    while(!juegoganado){
         if(agarrar != -1){
             if(jugador == 'y'){
                 cout<<"Jugador 1, Seleccione una columna: "<<endl;
                 jugador = 'r';
             }else{
                 cout<<"Jugador 2, Seleccione una columna: "<<endl;
-                jugador= 'y';
+                jugador= 'y';           
             }
         }
         while(true){
-            if(colocar == r*c)break;
+            if(colocar == f*c)break;
             cin>>columna;
             columna--;
             if(columna <= f && columna >= 0)break;
             else cout<<"El valor tiene que ser entre 1 y 7"<<endl;
             if(cin.fail()){
-                cin.clear;
+                cin.clear();
                 char d;
                 cin>>d;
              }
         }
-        if(colocar == r*c) break;
-        agarrar = 
+        if(colocar == f*c) break;
+        agarrar = soltar(columna, jugador, p, c);
+        if(agarrar == -1){ 
+            cout<<"La columna esta llena!\nIngrese otro numero entre 1 y 7: ";
+        }else{
+            juegoganado = revisar(agarrar,columna, p, f, c, piezas);
+            colocar++;  
+            imprimirtablero(p,f,c);
+        }    
+    }
+    if(colocar == f*c){
+        cout<<"No hay ganado! El juego quedo en empate\n";
+        return true;
+    }if(jugador == 'y'){
+        cout<<"El Jugador 2 es el gandor!\n";
+    }else{
+        cout<<"El Jugador 1 es el ganador!\n";
+    }
+    return true;
+}
+
+int soltar(int b, char jugador, juego p, int c){
+    if(b >= 0 && b<= c){
+        if(p.tablero[0][b] == ' '){
+            int i;
+            for(i = 0; p.tablero[i][b] == ' '; i++){
+                if(i==5){
+                    p.tablero[i][b] = jugador;
+                    return i;
+                }
+           }
+           i--;
+           p.tablero[i][b] = jugador;
+           return i;
+        }else{
+            return -1;
+        }
+    }else{
+        return -1;
     }
 }
-    
 
 
+bool revisar(int a, int b, juego p, int f, int c, int piezas){
+    int vertical = 1, horizontal = 1, diagonal_1=1, diagonal_2=1,i,j;
+    cout<<i<<" "<<b<<" "<<a<<endl;
+    char jugador = p.tablero[a][b];
+    cout<<jugador<<endl;
+    for(i = a + 1;p.tablero[i][b] ==jugador &&i<f;i++, vertical++);
+    for(i = a - 1;p.tablero[i][b] ==jugador &&i>=0;i--, vertical++);
+    if(vertical>=piezas){
+        return true;
+    }
+    for(j=b-1;p.tablero[a][j]==jugador&&j>=0;j--,horizontal++);
+    for(j=b+1;p.tablero[a][j]==jugador&&j<c;j++,horizontal++);
+}
