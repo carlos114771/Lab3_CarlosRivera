8b87f149c0540349654d185e4e662128fab00bf4
Carlos <alfreoldoriverac25@gmail.com>
2017-05-06 09:24:33 -0600

    creacion de la funcion jugar

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ struct juego{
     char p2;
 };
 void imprimirtablero(juego p, int f,int c);
-//bool jugar(juego p , bool juegoganado , int f, int c, int piezas);
+bool jugar(juego p , bool juegoganado , int f, int c, int piezas);
+bool revisar(int a, int b, juego p, int f, int c, int piezas);
+int soltar(int b, char jugador, juego p, int c);
+
 
 int main(int argc, char* argv[]){
     juego p;
@@ -50,6 +53,35 @@ void  imprimirtablero(juego p, int f, int c){
     
 }
 
-bool jugar();
+bool jugar(juego p, bool juegoganado, int f, int c, int piezas){
+    int columna, agarrar = 0; colocar=0;
+    char jugador = 'y';
+    while(!juegoterminado){
+        if(agarrar != -1){
+            if(jugador == 'y'){
+                cout<<"Jugador 1, Seleccione una columna: "<<endl;
+                jugador = 'r';
+            }else{
+                cout<<"Jugador 2, Seleccione una columna: "<<endl;
+                jugador= 'y';
+            }
+        }
+        while(true){
+            if(colocar == r*c)break;
+            cin>>columna;
+            columna--;
+            if(columna <= f && columna >= 0)break;
+            else cout<<"El valor tiene que ser entre 1 y 7"<<endl;
+            if(cin.fail()){
+                cin.clear;
+                char d;
+                cin>>d;
+             }
+        }
+        if(colocar == r*c) break;
+        agarrar = 
+    }
+}
+    
 
 
